e96d0e0dc0ae02d65027059dbfd0ea9fd91d13a1
terutora <tachi877teru@gmail.com>
2024-04-14 08:30:52 +0900

    commit

--- code_temp.py
+++ code_temp.py
@@ -28,4 +28,27 @@ list = r.json()
 tick_list = list["info"]
 codes = [d["Code"] for d in tick_list]
 new_list = [int(str(num)[:-1]) for num in codes]
-print(new_list)
+#print(new_list)
+
+from openpyxl import Workbook
+
+new_list = [1305,9997]
+
+for i_code in new_list:
+    a = requests.get(f"https://api.jquants.com/v1/fins/statements?code={i_code}", headers=headers)
+    st = a.json()
+    fin_list = st["statements"]
+
+    # 既存のExcelファイルを読み込む
+    existing_file = "out_st.xlsx"
+    writer = pd.ExcelWriter(existing_file, engine='openpyxl', mode='a')
+
+    # リストをDataFrameに変換
+    df = pd.DataFrame(fin_list)
+
+    # データを既存のExcelファイルに書き込む（シート名を指定して）
+    df.to_excel(writer, sheet_name=f"{i_code}", index=False)
+
+    # ファイルを保存してExcelWriterを閉じる
+    writer._save()
+    writer.close()
