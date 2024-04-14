3e66e8c42e574ab58d3361b4ea6626b593b61479
terutora <tachi877teru@gmail.com>
2024-04-15 02:13:00 +0900

    jquants_checkerフォルダに入れ替えました

--- jquants_checker/code_temp.py
+++ jquants_checker/code_temp.py
@@ -26,6 +26,33 @@ headers = {'Authorization': 'Bearer {}'.format(ID_TOKEN)}
 r = requests.get("https://api.jquants.com/v1/listed/info", headers=headers)
 list = r.json()
 tick_list = list["info"]
-codes = [d["Code"] for d in tick_list]
+
+exclude_key = 'MarketCodeName'  # 除外するキー
+exclude_value = 'その他'    # 除外する値
+
+# 特定のキーと値を含む辞書を除外する
+filtered_list = [d for d in tick_list if exclude_key not in d or d[exclude_key] != exclude_value]
+
+codes = [d["Code"] for d in filtered_list]
 new_list = [int(str(num)[:-1]) for num in codes]
-print(new_list)
+
+from openpyxl import Workbook
+
+for i_code in new_list:
+    a = requests.get(f"https://api.jquants.com/v1/fins/statements?code={i_code}", headers=headers)
+    st = a.json()
+    fin_list = st["statements"]
+
+    # 既存のExcelファイルを読み込む
+    existing_file = "out_st.xlsx"
+    writer = pd.ExcelWriter(existing_file, engine='openpyxl', mode='a')
+
+    # リストをDataFrameに変換
+    df = pd.DataFrame(fin_list)
+
+    # データを既存のExcelファイルに書き込む（シート名を指定して）
+    df.to_excel(writer, sheet_name=f"{i_code}", index=False)
+
+    # ファイルを保存してExcelWriterを閉じる
+    writer._save()
+    writer.close()
