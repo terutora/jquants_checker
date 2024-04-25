23de7e78fb4894b3e54f9bf7639f3141b16c94dc
terutora <tachi877teru@gmail.com>
2024-04-26 02:17:19 +0900

    test_sql

---
+++ test_sql.py
@@ -0,0 +1,67 @@
+import json
+import pandas as pd
+import requests
+import os
+from openpyxl import Workbook
+import mysql.connector
+
+config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Tachi877_mysql',
+    'database': 'finance_db'
+}
+
+conn = mysql.connector.connect(**config)
+cursor = conn.cursor()
+
+create_table_query = """
+CREATE TABLE IF NOT EXISTS stock_codes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code INT,
+    name VARCHAR(255),
+    market_code VARCHAR(255)
+)
+"""
+cursor.execute(create_table_query)
+
+"""
+# 特定の環境変数を取得
+MY_EMAIL = os.environ.get('my_email')
+PASS = os.environ.get('password')
+
+# リフレッシュトークンを取得
+resp = requests.post(
+    "https://api.jquants.com/v1/token/auth_user",
+    data=json.dumps({"mailaddress": MY_EMAIL, "password": PASS})
+)
+REFRESH_TOKEN = resp.json()["refreshToken"]
+
+# IDトークンを取得
+resp = requests.post(
+    "https://api.jquants.com/v1/token/auth_refresh",
+    params={"refreshtoken": REFRESH_TOKEN}
+)
+
+ID_TOKEN = resp.json()["idToken"]
+
+headers = {'Authorization': 'Bearer {}'.format(ID_TOKEN)}
+r = requests.get("https://api.jquants.com/v1/listed/info", headers=headers)
+list = r.json()
+tick_list = list["info"]
+
+exclude_key = 'MarketCodeName'  # 除外するキー
+exclude_value = 'その他'    # 除外する値
+
+# 特定のキーと値を含む辞書を除外する
+filtered_list = [d for d in tick_list if exclude_key not in d or d[exclude_key] != exclude_value]
+
+codes = [d["Code"] for d in filtered_list]
+new_list = [int(str(num)[:-1]) for num in codes]
+
+for i_code in new_list:
+    a = requests.get(f"https://api.jquants.com/v1/fins/statements?code={i_code}", headers=headers)
+    st = a.json()
+    fin_list = st["statements"]
+
+"""
