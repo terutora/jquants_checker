1ad2616eaf3d6ac37c2f37b0dc83692c55dad230
terutora <tachi877teru@gmail.com>
2024-05-01 23:05:16 +0900

    queriに（）がついているとエラーが起きます

--- print.py
+++ print.py
@@ -1,14 +1,6 @@
-import json
-import pandas as pd
-import requests
-import os
-from openpyxl import Workbook
-import mysql.connector
-
-# 特定の環境変数を取得
-MY_EMAIL = os.environ.get('my_email')
-PASS = os.environ.get('password')
-PASSWORD = os.environ.get('pass')
+marcer_list = input("Please enter the list of marcer: ").split()
+for marcer in marcer_list:
+    print(marcer + " TEXT,")
 
 config = {
     'host': 'localhost',
@@ -128,53 +120,3 @@ CREATE TABLE IF NOT EXISTS my_table (
     )
 """
 cursor.execute(create_table_query)
-
-
-# リフレッシュトークンを取得
-resp = requests.post(
-    "https://api.jquants.com/v1/token/auth_user",
-    data=json.dumps({"mailaddress": MY_EMAIL, "password": PASS})
-)
-REFRESH_TOKEN = resp.json()["refreshToken"]
-
-# IDトークンを取得
-resp = requests.post(
-    "https://api.jquants.com/v1/token/auth_refresh",
-    params={"refreshtoken": REFRESH_TOKEN}
-)
-
-ID_TOKEN = resp.json()["idToken"]
-
-headers = {'Authorization': 'Bearer {}'.format(ID_TOKEN)}
-r = requests.get("https://api.jquants.com/v1/listed/info", headers=headers)
-list = r.json()
-tick_list = list["info"]
-
-exclude_key = 'MarketCodeName'  # 除外するキー
-exclude_value = 'その他'    # 除外する値
-
-# 特定のキーと値を含む辞書を除外する
-filtered_list = [d for d in tick_list if exclude_key not in d or d[exclude_key] != exclude_value]
-
-codes = [d["Code"] for d in filtered_list]
-'''
-for i_code in codes:
-    a = requests.get(f"https://api.jquants.com/v1/fins/statements?code={i_code}", headers=headers)
-    st = a.json()
-    fin_list = st["statements"]
-'''
-a = requests.get(f"https://api.jquants.com/v1/fins/statements?code={1301}", headers=headers)
-st = a.json()
-f_list = st["statements"]
-fin_list = [item for item in f_list if 'REIT' not in item['KEY']]
-
-
-# リストのデータをテーブルに挿入
-for item in fin_list:
-    columns = ', '.join(item.keys())
-    placeholders = ', '.join(['%s'] * len(item))
-    insert_query = f"INSERT INTO code_info ({columns}) VALUES ({placeholders})"
-    cursor.execute(insert_query, tuple(item.values()))
-
-# コミットして変更を保存
-conn.commit()
