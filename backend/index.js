746085e5bf2a67d9e0702b41e38e2e1bcb1669b9
terutora <tachi877teru@gmail.com>
2024-06-16 14:20:51 +0900

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRoutes = require('./app/routes/apiRoutes.js');
-
+const path = require('path');
 const app = express();
 const cors = require('cors');
 
@@ -9,6 +9,14 @@ app.use(cors());
 
 // ミドルウェアの設定やルーティングの設定
 app.use('/api', userRoutes);
+
+// 静的ファイルの設定
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// その他のリクエストに対してはindex.htmlを返す
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+});
 
 // サーバーを起動
 const PORT = process.env.PORT || 4000;
