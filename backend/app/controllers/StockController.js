9c7d0ac2de7ec20270ee5bcaeb807f7b8365980b
terutora <tachi877teru@gmail.com>
2024-06-17 23:50:20 +0900

    ssl

--- backend/app/controllers/StockController.js
+++ backend/app/controllers/StockController.js
@@ -8,7 +8,10 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(process.env.DB_SSL_CERT)
+  }
 });
 
 connection.connect(err => {
