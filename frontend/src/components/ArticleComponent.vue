e6ebbe2936bb9e545e10e55f38478428b7881f47
terutora <tachi877teru@gmail.com>
2024-06-17 14:05:47 +0900

    commit

--- frontend/src/components/ArticleComponent.vue
+++ frontend/src/components/ArticleComponent.vue
@@ -2,6 +2,9 @@
   <div :id="article.id">
     <h2>{{ article.year }}</h2>
     <p>{{ article.text }}</p>
+    <ul>
+      <li v-for="tag in article.tags" :key="tag.id">{{ tag.name }}</li>
+    </ul>
   </div>
 </template>
 
